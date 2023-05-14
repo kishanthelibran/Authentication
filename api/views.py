3aa42a3257c8dfbeff80d3314e66aedeaf0e98dc
Kishan <kishan@learningproject.com>
2023-05-14 22:50:53 +0530

    final changes

--- api/views.py
+++ api/views.py
@@ -30,7 +30,7 @@ def RegisterUser(request):
                      dept_id=dept, role_id=role, salary=salary)
     user.save()'''
 
-    return Response("Data successfully entered")
+    return Response("successfully entered")
 
 
 @api_view(['POST'])
